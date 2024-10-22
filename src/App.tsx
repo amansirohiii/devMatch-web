e5d386ae30a47f0472f59181c6fa7ad8154fb049
Aman Sirohi <amansirohi077@gmail.com>
2024-10-22 14:10:00 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
     return (
         <>
+        <Navbar/>
             <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </>
     );
