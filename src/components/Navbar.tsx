55bb497672729570cfc07613a9467ec61dfd947a
Aman Sirohi <amansirohi077@gmail.com>
2024-10-30 22:00:12 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = () => {
         <>
         <span className="sm:hidden">
         <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signjup">Signup</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
         </span></>}
       </ul>
     </div>
